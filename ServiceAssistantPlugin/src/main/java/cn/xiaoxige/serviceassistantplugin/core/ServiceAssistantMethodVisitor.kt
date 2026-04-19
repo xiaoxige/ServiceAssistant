c39ce5657bea61c6ea3d5f8a8933a35fbf742a08
xiaoxige <xiaoxigexiaoan@outlook.com>
2026-04-19 17:05:50 +0800

    优化注入的 asm 写法

--- ServiceAssistantPlugin/src/main/java/cn/xiaoxige/serviceassistantplugin/core/ServiceAssistantMethodVisitor.kt
+++ ServiceAssistantPlugin/src/main/java/cn/xiaoxige/serviceassistantplugin/core/ServiceAssistantMethodVisitor.kt
@@ -4,15 +4,9 @@ import cn.xiaoxige.serviceassistantplugin.constant.ServiceAssistantConstant
 import org.objectweb.asm.Label
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Type
 import org.objectweb.asm.commons.AdviceAdapter
 
-/**
- * @author xiaoxige
- * @date 4/5/21 12:16 AM
- * -
- * email: xiaoxigexiaoan@outlook.com
- * desc: 方法访问
- */
 class ServiceAssistantMethodVisitor(
     methodVisitor: MethodVisitor,
     private val visitorClassName: String,
@@ -26,33 +20,38 @@ class ServiceAssistantMethodVisitor(
     override fun onMethodEnter() {
         super.onMethodEnter()
 
-        mv.visitInsn(ACONST_NULL)
-        mv.visitVarInsn(ALOAD, 0)
+        val labelSkip = Label()
+        val labelInject = Label()
+
+        // if (mIsAutoInit == null) goto inject
+        mv.visitInsn(Opcodes.ACONST_NULL)
+        loadThis()
         mv.visitFieldInsn(
-            GETFIELD,
+            Opcodes.GETFIELD,
             visitorClassName,
             isAutoInitFieldName,
             ServiceAssistantConstant.SIGNATURE_BOOLEAN
         )
-        val label1 = Label()
-        mv.visitJumpInsn(IF_ACMPEQ, label1)
-        mv.visitVarInsn(ALOAD, 0)
+        mv.visitJumpInsn(Opcodes.IF_ACMPEQ, labelInject)
+
+        // if (mIsAutoInit.booleanValue()) goto skip
+        loadThis()
         mv.visitFieldInsn(
-            GETFIELD,
+            Opcodes.GETFIELD,
             visitorClassName,
             isAutoInitFieldName,
             ServiceAssistantConstant.SIGNATURE_BOOLEAN
         )
         mv.visitMethodInsn(
-            INVOKEVIRTUAL,
+            Opcodes.INVOKEVIRTUAL,
             ServiceAssistantConstant.PATH_BOOLEAN,
             ServiceAssistantConstant.NAME_BOOLEAN,
             ServiceAssistantConstant.DESC_RETURN_BOOLEAN,
             false
         )
-        val label2 = Label()
-        mv.visitJumpInsn(IFNE, label2)
-        mv.visitLabel(label1)
+        mv.visitJumpInsn(Opcodes.IFNE, labelSkip)
+
+        mv.visitLabel(labelInject)
 
         fieldInfo.keys.forEach {
             val value = fieldInfo[it]
@@ -60,75 +59,83 @@ class ServiceAssistantMethodVisitor(
             insertInjectedProducer(it, value)
         }
 
-        mv.visitVarInsn(ALOAD, 0)
-        mv.visitInsn(ICONST_1)
+        // this.mIsAutoInit = Boolean.valueOf(true)
+        loadThis()
+        mv.visitInsn(Opcodes.ICONST_1)
         mv.visitMethodInsn(
-            INVOKESTATIC,
+            Opcodes.INVOKESTATIC,
             ServiceAssistantConstant.PATH_BOOLEAN,
             ServiceAssistantConstant.DESC_VALUE_OF,
             ServiceAssistantConstant.DESC_RETURN_BOOLEAN_FULL,
             false
         )
         mv.visitFieldInsn(
-            PUTFIELD,
+            Opcodes.PUTFIELD,
             visitorClassName,
             isAutoInitFieldName,
             ServiceAssistantConstant.SIGNATURE_BOOLEAN
         )
-        mv.visitLabel(label2)
 
+        mv.visitLabel(labelSkip)
     }
 
     private fun insertInjectedProducer(name: String, info: Pair<String, String>) {
         val injectedInterface = info.first
         val targetInterfaceProducer = ServiceAssistantConstant.getInjectedProducerClassFullName(
-            injectedInterface.substring(
-                1,
-                injectedInterface.length - 1
-            )
+            injectedInterface.substring(1, injectedInterface.length - 1)
         )
 
-        val label0 = Label()
-        val label1 = Label()
-        val label2 = Label()
-        mv.visitTryCatchBlock(label0, label1, label2, ServiceAssistantConstant.PATH_EXCEPTION)
-        mv.visitLabel(label0)
-        mv.visitLineNumber(33, label0)
+        val tryStart = Label()
+        val tryEnd = Label()
+        val catchStart = Label()
+        val afterCatch = Label()
+
+        mv.visitTryCatchBlock(
+            tryStart, tryEnd, catchStart,
+            ServiceAssistantConstant.PATH_EXCEPTION
+        )
+        mv.visitLabel(tryStart)
+
+        // Class.forName(producerClassName)
         mv.visitLdcInsn(targetInterfaceProducer.replace("/", "."))
         mv.visitMethodInsn(
-            INVOKESTATIC,
+            Opcodes.INVOKESTATIC,
             ServiceAssistantConstant.PATH_CLASS,
             ServiceAssistantConstant.DESC_FOR_NAME,
             ServiceAssistantConstant.SIGNATURE_STRING_CLASS,
             false
         )
-        mv.visitInsn(POP)
-        val label3 = Label()
-        mv.visitLabel(label3)
-        mv.visitLineNumber(34, label3)
-        mv.visitVarInsn(ALOAD, 0)
+        mv.visitInsn(Opcodes.POP)
+
+        // this.field = XxxProducer.getInstance(sign)
+        loadThis()
+        mv.visitLdcInsn(info.second)
         mv.visitMethodInsn(
-            INVOKESTATIC,
+            Opcodes.INVOKESTATIC,
             targetInterfaceProducer,
             ServiceAssistantConstant.NAME_GET_TARGET_INSTANCE_METHOD,
-            "()$injectedInterface",
+            "(Ljava/lang/String;)$injectedInterface",
             false
         )
         mv.visitFieldInsn(
-            PUTFIELD,
+            Opcodes.PUTFIELD,
             visitorClassName,
             name,
             injectedInterface
         )
-        mv.visitLabel(label1)
-        mv.visitLineNumber(36, label1)
-        val label4 = Label()
-        mv.visitJumpInsn(GOTO, label4)
-        mv.visitLabel(label2)
-        mv.visitLineNumber(35, label2)
-        mv.visitFrame(F_SAME1, 0, null, 1, arrayOf<Any>(ServiceAssistantConstant.PATH_EXCEPTION))
-        mv.visitVarInsn(ASTORE, 1)
-        mv.visitLabel(label4)
-    }
 
+        mv.visitLabel(tryEnd)
+        mv.visitJumpInsn(Opcodes.GOTO, afterCatch)
+
+        // catch (Exception e) { /* ignore */ }
+        mv.visitLabel(catchStart)
+        mv.visitFrame(
+            Opcodes.F_SAME1, 0, null,
+            1, arrayOf<Any>(ServiceAssistantConstant.PATH_EXCEPTION)
+        )
+        val exceptionLocal = newLocal(Type.getType("Ljava/lang/Exception;"))
+        mv.visitVarInsn(Opcodes.ASTORE, exceptionLocal)
+
+        mv.visitLabel(afterCatch)
+    }
 }
